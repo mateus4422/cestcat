c6b09c8deb16b05b2e92ba84a506d9a86c91978c
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-04-18 11:10:26 -0300

    cest.py

--- cest.py
+++ cest.py
@@ -1,29 +1,11 @@
-import os
 import pandas as pd
 import streamlit as st
 import base64
 
 
-
-def save_uploaded_files(uploaded_files, save_path):
-    file_paths = []
-    for file in uploaded_files:
-        with open(os.path.join(save_path, file.name), 'wb') as f:
-            f.write(file.getvalue())
-        file_paths.append(os.path.join(save_path, file.name))
-    return file_paths
-save_path = '/path/to/save/files'
-uploaded_files = st.file_uploader("Selecione os arquivos xlsx", type=["xlsx"], accept_multiple_files=True)
-if uploaded_files:
-    file_paths = save_uploaded_files(uploaded_files, save_path)
-    # Restante do código aqui...
-
-def load_data(file_paths):
-    data = {}
-    for file_path in file_paths:
-        sheets = pd.read_excel(file_path, sheet_name=None, engine='openpyxl')
-        data[file_path] = sheets
-    return data
+def load_data(file):
+    sheets = pd.read_excel(file, sheet_name=None, engine='openpyxl')
+    return sheets
 
 
 def get_file_date(file_name):
@@ -38,19 +20,17 @@ def get_file_date(file_name):
 
 def search_cest(data, cest_code):
     result = []
-    for file_path, sheets in data.items():
-        for sheet_name, df in sheets.items():
-            matching_rows = df[df.apply(lambda row: row.astype(str).str.contains(cest_code).any(), axis=1)]
-            if not matching_rows.empty:
-                matching_rows['Arquivo'] = os.path.basename(file_path)
-                matching_rows['CEST'] = cest_code
-                matching_rows['Categoria'] = sheet_name
-                result.append(matching_rows)
+    for sheet_name, df in data.items():
+        matching_rows = df[df.apply(lambda row: row.astype(str).str.contains(cest_code).any(), axis=1)]
+        if not matching_rows.empty:
+            matching_rows['Arquivo'] = file.name
+            matching_rows['CEST'] = cest_code
+            matching_rows['Categoria'] = sheet_name
+            result.append(matching_rows)
 
     if result:
         final_df = pd.concat(result, ignore_index=True)
-        final_df['Data'] = final_df['Arquivo'].str.replace('.xlsx', '').str.replace('.', '/')
-        final_df['Data'] = pd.to_datetime(final_df['Data'], format='%d/%m/%Y')
+        final_df['Data'] = get_file_date(file.name)
         final_df['MVA ST 1'] = final_df['Unnamed: 10'] * 100
         final_df['aliquota'] = final_df['Unnamed: 12'] * 100
         final_df = final_df[['Data', 'Categoria', 'CEST', 'MVA ST 1', 'aliquota']]
@@ -66,10 +46,9 @@ def search_cest(data, cest_code):
 
 st.title("Pesquisa de CEST")
 
-uploaded_files = st.file_uploader("Selecione os arquivos xlsx", type=["xlsx"], accept_multiple_files=True)
-if uploaded_files:
-    file_paths = save_uploaded_files(uploaded_files)
-    data = load_data(file_paths)
+file = st.sidebar.file_input("Selecione o arquivo xlsx", type=["xlsx"])
+if file:
+    data = load_data(file)
 
     cest_code = st.text_input("Digite o código CEST")
     if st.button("Pesquisar"):
@@ -79,5 +58,3 @@ if uploaded_files:
         b64 = base64.b64encode(csv.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="resultado.csv">Exportar resultado</a>'
         st.markdown(href, unsafe_allow_html=True)
-
-
