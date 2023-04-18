ab5bd52e78c22cd9ec8f18eec2df45f1eacdc510
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-04-18 11:28:22 -0300

    Update cest.py

--- cest.py
+++ cest.py
@@ -1,36 +1,38 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 import base64
+import os
 
+def save_uploaded_files(uploaded_files):
+    file_paths = []
+    for file in uploaded_files:
+        with open(os.path.join(file.name), 'wb') as f:
+            f.write(file.getvalue())
+        file_paths.append(os.path.join(file.name))
+    return file_paths
 
-def load_data(file):
-    sheets = pd.read_excel(file, sheet_name=None, engine='openpyxl')
-    return sheets
-
-
-def get_file_date(file_name):
-    date_str = file_name.split('.')[0]
-    date_str = date_str.replace('.', '/')
-    try:
-        date = pd.to_datetime(date_str, format='%d/%m/%Y').date()
-    except ValueError:
-        date = None
-    return date
-
+def load_data(file_paths):
+    data = {}
+    for file_path in file_paths:
+        sheets = pd.read_excel(file_path, sheet_name=None, engine='openpyxl')
+        data[file_path] = sheets
+    return data
 
 def search_cest(data, cest_code):
     result = []
-    for sheet_name, df in data.items():
-        matching_rows = df[df.apply(lambda row: row.astype(str).str.contains(cest_code).any(), axis=1)]
-        if not matching_rows.empty:
-            matching_rows['Arquivo'] = file.name
-            matching_rows['CEST'] = cest_code
-            matching_rows['Categoria'] = sheet_name
-            result.append(matching_rows)
+    for file_path, sheets in data.items():
+        for sheet_name, df in sheets.items():
+            matching_rows = df[df.apply(lambda row: row.astype(str).str.contains(cest_code).any(), axis=1)]
+            if not matching_rows.empty:
+                matching_rows['Arquivo'] = os.path.basename(file_path)
+                matching_rows['CEST'] = cest_code
+                matching_rows['Categoria'] = sheet_name
+                result.append(matching_rows)
 
     if result:
         final_df = pd.concat(result, ignore_index=True)
-        final_df['Data'] = get_file_date(file.name)
+        final_df['Data'] = final_df['Arquivo'].str.replace('.xlsx', '').str.replace('.', '/')
+        final_df['Data'] = pd.to_datetime(final_df['Data'], format='%d/%m/%Y')
         final_df['MVA ST 1'] = final_df['Unnamed: 10'] * 100
         final_df['aliquota'] = final_df['Unnamed: 12'] * 100
         final_df = final_df[['Data', 'Categoria', 'CEST', 'MVA ST 1', 'aliquota']]
@@ -43,18 +45,38 @@ def search_cest(data, cest_code):
         columns = ['Data', 'Categoria', 'CEST', 'MVA ST 1', 'aliquota']
         return pd.DataFrame(columns=columns)
 
+def export_result(result_df):
+    csv = result_df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="resultado.csv">Exportar resultado</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
-st.title("Pesquisa de CEST")
+st.set_page_config(page_title="Pesquisa de CEST", page_icon=":mag:")
 
-file = st.sidebar.file_input("Selecione o arquivo xlsx", type=["xlsx"])
-if file:
-    data = load_data(file)
+
+st.header("Upload de arquivos")
+st.write("Por favor, faça o upload dos arquivos que deseja pesquisar.")
+
+uploaded_files = st.file_uploader("Selecione os arquivos xlsx", type=["xlsx"], accept_multiple_files=True)
+
+if uploaded_files:
+
+    file_paths = save_uploaded_files(uploaded_files)
+    data = load_data(file_paths)
+
 
     cest_code = st.text_input("Digite o código CEST")
     if st.button("Pesquisar"):
         search_result = search_cest(data, cest_code)
-        st.dataframe(search_result)
-        csv = search_result.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="resultado.csv">Exportar resultado</a>'
-        st.markdown(href, unsafe_allow_html=True)
+        if not search_result.empty:
+            st.dataframe(search_result)
+
+
+            export_result(search_result)
+        else:
+            st.warning("Nenhum resultado encontrado para o código CEST informado.")
+
+    st.markdown("---")
+
+    st.write("Desenvolvido por [Mateus Ramos](https://www.linkedin.com/in/mateusramosb/)")
+
