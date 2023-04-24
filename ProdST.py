0d819fdd8f13ca9e34911e3f37cc629e30d28321
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-04-24 10:43:55 -0300

    Add files via upload

---
+++ ProdST.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+
+def tb_produtos():
+# Carregue o arquivo Excel da URL
+    url = 'https://github.com/mateus4422/cestcat/raw/cestcat/Planilha%20de%20Produtos.xlsx'
+    df = pd.read_excel(url, engine='openpyxl')
+
+    st.title("Tabela de Produtos")
+
+    # Filtros para pesquisar por COD_PRODUTO e NCM
+    cod_produto = st.text_input("Digite o código do produto (COD_PRODUTO):")
+    ncm = st.text_input("Digite o NCM:")
+
+    # Filtrar o dataframe com base nos valores inseridos
+    filtered_df = df[df['COD_PRODUTO'].astype(str).str.contains(cod_produto) & df['NCM'].astype(str).str.contains(ncm)]
+
+    # Obtenha os valores únicos na coluna PROD_ST
+    unique_prod_st = df['PROD_ST'].unique()
+    unique_prod_st.sort()
+
+    # Crie um filtro de seleção usando o widget selectbox do Streamlit
+    selected_prod_st = st.selectbox("Produto ST?:", unique_prod_st)
+
+    # Filtrar o dataframe usando o valor selecionado
+    filtered_df = filtered_df[filtered_df['PROD_ST'] == selected_prod_st]
+
+    st.write(f"Resultados para COD_PRODUTO: {cod_produto}, NCM: {ncm}, e PROD_ST: {selected_prod_st}")
+    st.dataframe(filtered_df)
