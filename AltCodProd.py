ae9814b273cf428743268c839bc8eb650d714fcc
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-04-25 13:26:40 -0300

    Update AltCodProd.py

--- AltCodProd.py
+++ AltCodProd.py
@@ -13,27 +13,27 @@ def altcodprod()
     df = pd.read_excel(xls_file, engine="openpyxl")
     return df
 
-# Carrega os dados
-url = "https://github.com/mateus4422/cestcat/raw/cestcat/Tabela%20de%20código.xlsx"
-data = load_data(url)
+        # Carrega os dados
+        url = "https://github.com/mateus4422/cestcat/raw/cestcat/Tabela%20de%20código.xlsx"
+        data = load_data(url)
 
-# Converte os códigos de compra e venda para inteiros
-data['Código de Compra'] = data['Código de Compra'].astype(int)
-data['Código de Venda'] = data['Código de Venda'].astype(int)
+        # Converte os códigos de compra e venda para inteiros
+        data['Código de Compra'] = data['Código de Compra'].astype(int)
+        data['Código de Venda'] = data['Código de Venda'].astype(int)
 
-st.title("Visualização de arquivo XLSX")
+        st.title("Visualização de arquivo XLSX")
 
-# Filtros na mesma janela da tabela
-ncm_filter = st.text_input("Filtrar por NCM:")
-ean_filter = st.text_input("Filtrar por EAN:")
+        # Filtros na mesma janela da tabela
+        ncm_filter = st.text_input("Filtrar por NCM:")
+        ean_filter = st.text_input("Filtrar por EAN:")
 
-# Aplica o filtro de NCM
-if ncm_filter:
-    data = data[data["NCM"].astype(str).str.contains(ncm_filter)]
+        # Aplica o filtro de NCM
+        if ncm_filter:
+            data = data[data["NCM"].astype(str).str.contains(ncm_filter)]
 
-# Aplica o filtro de EAN
-if ean_filter:
-    data = data[data["EAN de Compra"].astype(str).str.contains(ean_filter)]
+        # Aplica o filtro de EAN
+        if ean_filter:
+            data = data[data["EAN de Compra"].astype(str).str.contains(ean_filter)]
 
-# Exibe a tabela
-st.write(data)
+        # Exibe a tabela
+        st.write(data)
