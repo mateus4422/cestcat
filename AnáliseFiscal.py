fbd8cc69e28bf1139f07dd572a65d5969b924993
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-04-28 16:56:11 -0300

    Add files via upload

---
+++ AnáliseFiscal.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import requests
+from io import BytesIO
+from openpyxl import load_workbook
+
+# Título da aplicação
+st.title("Análise Fiscal")
+
+# Solicitar o link raw do arquivo do GitHub
+url = st.text_input("https://github.com/mateus4422/cestcat/raw/cestcat/Cabeçalho_Análise.xlsx")
+
+def analisefiscal():
+    def load_xlsx_from_url(url):
+        response = requests.get(url)
+        content = response.content
+        wb = load_workbook(BytesIO(content), read_only=True)
+        sheet_name = wb.sheetnames[0]
+        ws = wb[sheet_name]
+        data = ws.values
+        columns = next(data)[0:]
+        df = pd.DataFrame(data, columns=columns)
+        return df
+    
+    # Carregar e exibir a planilha xlsx
+    if url:
+        try:
+            df = load_xlsx_from_url(url)
+            st.write(df)
+        except Exception as e:
+            st.error(f"Erro ao carregar o arquivo xlsx: {e}")
