5c0ce6c1f161c33fb4641a4ce0884b2d7f477bb8
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-04-30 16:22:56 -0300

    Update EstruturaComplementar.py

--- EstruturaComplementar.py
+++ EstruturaComplementar.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-def e struturacomplementar():
+def estruturacomplementar():
     def converter_para_inteiro(x):
         if pd.isna(x):
             return None
