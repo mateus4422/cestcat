2565c0ddfce2055d35824f8575d2469ba87ce1aa
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-03 14:43:17 -0300

    Update cest.py

--- cest.py
+++ cest.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 st.set_page_config(page_title="Pesquisa de CEST", page_icon=":mag:")
 
-def cest():
+def cest ():
     def save_uploaded_files(uploaded_files):
         file_paths = []
         for file in uploaded_files:
@@ -15,6 +15,7 @@ def cest():
             file_paths.append(os.path.join(file.name))
         return file_paths
 
+
     def load_data(file_paths):
         data = {}
         for file_path in file_paths:
@@ -22,8 +23,6 @@ def cest():
             data[file_path] = sheets
         return data
 
-    def find_columns(df, keyword):
-        return [col for col in df.columns if keyword.lower() in col.lower()]
 
     def search_cest(data, cest_code):
         result = []
@@ -40,59 +39,49 @@ def cest():
             final_df = pd.concat(result, ignore_index=True)
             final_df['Data'] = final_df['Arquivo'].str.replace('.xlsx', '').str.replace('.', '/')
             final_df['Data'] = pd.to_datetime(final_df['Data'], format='%d/%m/%Y')
-
-            mva_columns = find_columns(final_df, 'MVA')
-            if mva_columns:
-                final_df['MVA ST 1'] = final_df[mva_columns[0]] * 100
-            else:
-                final_df['MVA ST 1'] = None
-
-            aliquota_columns = find_columns(final_df, 'aliquota')
-            if aliquota_columns:
-                final_df['aliquota'] = final_df[aliquota_columns[0]] * 100
-            else:
-                final_df['aliquota'] = None
-
-            final_df['MVA ST 1'] = final_df['MVA ST 1'].apply(lambda x: x / 100 if isinstance(x, (int, float)) else x)
-            final_df['aliquota'] = final_df['aliquota'].apply(lambda x: x / 100 if isinstance(x, (int, float)) else x)
+            final_df['MVA ST 1'] = final_df['Unnamed: 10'] * 100
+            final_df['aliquota'] = final_df['Unnamed: 12'] * 100
+            final_df = final_df[['Data', 'Categoria', 'CEST', 'MVA ST 1', 'aliquota']]
+            final_df = final_df.sort_values('Data', ascending=False)
+            final_df[['MVA ST 1', 'aliquota']] /= 100
             final_df[['MVA ST 1', 'aliquota']] = final_df[['MVA ST 1', 'aliquota']].applymap('{:.2%}'.format)
             final_df = final_df[['Data', 'Categoria', 'CEST', 'MVA ST 1', 'aliquota']]
             return final_df
         else:
             columns = ['Data', 'Categoria', 'CEST', 'MVA ST 1', 'aliquota']
             return pd.DataFrame(columns=columns)
-
     def export_result(result_df):
         csv = result_df.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="resultado.csv">Exportar resultado</a>'
         st.markdown(href, unsafe_allow_html=True)
 
-        def plot_mva_changes(result_df):
-            fig = px.line(result_df, x='Data', y='MVA ST 1', title='Mudanças no MVA ST 1 ao longo do tempo')
-            fig.update_traces(mode='markers+lines')
-            fig.update_xaxes(title_text='Data')
-            fig.update_yaxes(title_text='MVA ST 1', tickformat='.2%')
-            st.plotly_chart(fig)
+    def plot_mva_changes(result_df):
+        fig = px.line(result_df, x='Data', y='MVA ST 1', title='Mudanças no MVA ST 1 ao longo do tempo')
+        fig.update_traces(mode='markers+lines')
+        fig.update_xaxes(title_text='Data')
+        fig.update_yaxes(title_text='MVA ST 1', tickformat='.2%')
+        st.plotly_chart(fig)
 
-        st.header("Upload de arquivos")
-        st.write("Por favor, faça o upload dos arquivos que deseja pesquisar.")
+    st.header("Upload de arquivos")
+    st.write("Por favor, faça o upload dos arquivos que deseja pesquisar.")
 
-        uploaded_files = st.file_uploader("Selecione os arquivos xlsx", type=["xlsx"], accept_multiple_files=True)
+    uploaded_files = st.file_uploader("Selecione os arquivos xlsx", type=["xlsx"], accept_multiple_files=True)
 
-        if uploaded_files:
-            file_paths = save_uploaded_files(uploaded_files)
-            data = load_data(file_paths)
+    if uploaded_files:
 
-            cest_code = st.text_input("Digite o código CEST")
-            if st.button("Pesquisar"):
-                search_result = search_cest(data, cest_code)
-                if not search_result.empty:
-                    st.dataframe(search_result)
-                    plot_mva_changes(search_result)
-                    export_result(search_result)
-                else:
-                    st.write("Desenvolvido por [Mateus Ramos](https://www.linkedin.com/in/mateusramosb/)")
+        file_paths = save_uploaded_files(uploaded_files)
+        data = load_data(file_paths)
 
-    cest()
+        cest_code = st.text_input("Digite o código CEST")
+        if st.button("Pesquisar"):
+            search_result = search_cest(data, cest_code)
+            if not search_result.empty:
+                st.dataframe(search_result)
 
+                # Adicione esta chamada de função logo após exibir o dataframe
+                plot_mva_changes(search_result)
+
+                export_result(search_result)
+            else:
+                st.write("Desenvolvido por [Mateus Ramos](https://www.linkedin.com/in/mateusramosb/)")
