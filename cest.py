a9292e0ce4d354b63e93d4ea6b5dc36eb35eeddd
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-03 14:47:45 -0300

    Update cest.py

--- cest.py
+++ cest.py
@@ -23,31 +23,30 @@ def cest ():
             data[file_path] = sheets
         return data
 
-
     def search_cest(data, cest_code):
-    result = []
-    for file_path, sheets in data.items():
-        for sheet_name, df in sheets.items():
-            matching_rows = df[df.apply(lambda row: row.astype(str).str.contains(cest_code).any(), axis=1)]
-            if not matching_rows.empty:
-                matching_rows['Arquivo'] = os.path.basename(file_path)
-                matching_rows['CEST'] = cest_code
-                matching_rows['Categoria'] = sheet_name
-                result.append(matching_rows)
+        result = []
+        for file_path, sheets in data.items():
+            for sheet_name, df in sheets.items():
+                matching_rows = df[df.apply(lambda row: row.astype(str).str.contains(cest_code).any(), axis=1)]
+                if not matching_rows.empty:
+                    matching_rows['Arquivo'] = os.path.basename(file_path)
+                    matching_rows['CEST'] = cest_code
+                    matching_rows['Categoria'] = sheet_name
+                    result.append(matching_rows)
 
-    if result:
-        final_df = pd.concat(result, ignore_index=True)
-        final_df['Data'] = final_df['Arquivo'].str.replace('.xlsx', '').str.replace('.', '/')
-        final_df['Data'] = pd.to_datetime(final_df['Data'], format='%d/%m/%Y')
-        final_df['MVA ST 1'] = final_df['Unnamed: 10'] * 100
-        final_df['aliquota'] = final_df['Unnamed: 12'] * 100
-        final_df = final_df.sort_values('Data', ascending=False)
-        final_df[['MVA ST 1', 'aliquota']] /= 100
-        final_df[['MVA ST 1', 'aliquota']] = final_df[['MVA ST 1', 'aliquota']].applymap('{:.2%}'.format)
-        return final_df
-    else:
-        columns = list(df.columns) + ['Arquivo', 'CEST', 'Categoria']
-        return pd.DataFrame(columns=columns)
+        if result:
+            final_df = pd.concat(result, ignore_index=True)
+            final_df['Data'] = final_df['Arquivo'].str.replace('.xlsx', '').str.replace('.', '/')
+            final_df['Data'] = pd.to_datetime(final_df['Data'], format='%d/%m/%Y')
+            final_df['MVA ST 1'] = final_df['Unnamed: 10'] * 100
+            final_df['aliquota'] = final_df['Unnamed: 12'] * 100
+            final_df = final_df.sort_values('Data', ascending=False)
+            final_df[['MVA ST 1', 'aliquota']] /= 100
+            final_df[['MVA ST 1', 'aliquota']] = final_df[['MVA ST 1', 'aliquota']].applymap('{:.2%}'.format)
+            return final_df
+        else:
+            columns = list(df.columns) + ['Arquivo', 'CEST', 'Categoria']
+            return pd.DataFrame(columns=columns)
 
     def export_result(result_df):
         csv = result_df.to_csv(index=False)
@@ -74,14 +73,13 @@ def cest ():
 
         cest_code = st.text_input("Digite o código CEST")
         if st.button("Pesquisar"):
-    search_result = search_cest(data, cest_code)
-    if not search_result.empty:
-        st.dataframe(search_result)
+            search_result = search_cest(data, cest_code)
+            if not search_result.empty:
+                st.dataframe(search_result)
 
-        # Adicione esta chamada de função logo após exibir o dataframe
-        plot_mva_changes(search_result)
+                # Adicione esta chamada de função logo após exibir o dataframe
+                plot_mva_changes(search_result)
 
-        export_result(search_result)
-    else:
-        st.write("Desenvolvido por [Mateus Ramos](https://www.linkedin.com/in/mateusramosb/)")
-
+                export_result(search_result)
+            else:
+                st.write("Desenvolvido por [Mateus Ramos](https://www.linkedin.com/in/mateusramosb/)")
