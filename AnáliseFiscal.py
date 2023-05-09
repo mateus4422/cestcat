89e5dddca6aed9b9e4d51eb1e4acb15142b92ee5
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-09 11:45:54 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import streamlit as st
 
-# URL do link raw do arquivo Excel no GitHub
-url = "https://github.com/mateus4422/cestcat/raw/cestcat/Cabe%C3%A7alho_An%C3%A1lise.xlsx"
 
-# Carregue a planilha em um DataFrame do Pandas
-df = pd.read_excel(url, engine='openpyxl')
+def cabecalhoanalise():
+    # URL do link raw do arquivo Excel no GitHub
+    url = "https://github.com/mateus4422/cestcat/raw/cestcat/Cabe%C3%A7alho_An%C3%A1lise.xlsx"
 
-# Exiba apenas o cabeçalho da planilha no Streamlit
-st.write("Cabeçalho da planilha de análise fiscal:")
-st.write(df.head(0))
+    # Carregue a planilha em um DataFrame do Pandas
+    df = pd.read_excel(url, engine='openpyxl')
 
-# Execute o script com o comando `streamlit run nome_do_arquivo.py`
+    # Exiba apenas o cabeçalho da planilha no Streamlit
+    st.write("Cabeçalho da planilha de análise fiscal:")
+    st.write(df.head(0))
+
+    # Execute o script com o comando `streamlit run nome_do_arquivo.py`
