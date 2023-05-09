5c883a3a40b2f837f66cd2d8fc7086a52b95f21f
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-09 11:40:54 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -1,30 +1,14 @@
+import pandas as pd
 import streamlit as st
-from openpyxl import load_workbook
-from io import BytesIO
-import requests
 
-def analisefiscal():
-    # Título da aplicação
-    st.title("Análise Fiscal")
+# URL do link raw do arquivo Excel no GitHub
+url = "https://github.com/mateus4422/cestcat/raw/cestcat/Cabe%C3%A7alho_An%C3%A1lise.xlsx"
 
-    # Nome do arquivo da planilha
-    file_name = "Cabeçalho_Análise.xlsx"
+# Carregue a planilha em um DataFrame do Pandas
+df = pd.read_excel(url, engine='openpyxl')
 
-    # Montar a URL do arquivo no GitHub
-    url = f"https://github.com/mateus4422/cestcat/raw/cestcat/{file_name}"
+# Exiba apenas o cabeçalho da planilha no Streamlit
+st.write("Cabeçalho da planilha de análise fiscal:")
+st.write(df.head(0))
 
-    try:
-        # Fazer o download do arquivo usando a biblioteca requests
-        response = requests.get(url)
-        content = response.content
-
-        # Carregar a planilha usando a biblioteca openpyxl
-        wb = load_workbook(filename=BytesIO(content), read_only=True)
-        ws = wb.active
-
-        # Ler os dados da planilha e exibi-los no Streamlit
-        data = [[cell.value for cell in row] for row in ws.iter_rows()]
-        st.table(data)
-
-    except Exception as e:
-        st.error(f"Erro ao carregar o arquivo xlsx: {e}")
+# Execute o script com o comando `streamlit run nome_do_arquivo.py`
