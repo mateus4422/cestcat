a73e1c4aa4904f6344029cf216b9e7f8f40944ef
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-09 15:18:58 -0300

    Update AltCodProd.py

--- AltCodProd.py
+++ AltCodProd.py
@@ -27,16 +27,24 @@ def altcodprod():
     # Filtros na mesma janela da tabela
     ncm_filter = st.text_input("Filtrar por NCM:")
     ean_filter = st.text_input("Filtrar por EAN:")
-    cod_venda_format = st.text_input("Formato do Código de Venda (Ex: ###-#####):")
-    cod_compra_format = st.text_input("Formato do Código de Compra (Ex: #####):")
+    cod_venda_type = st.selectbox("Tipo de dado para Código de Venda:", ("", "Inteiro", "Decimal", "Data"))
+    cod_compra_type = st.selectbox("Tipo de dado para Código de Compra:", ("", "Inteiro", "Decimal", "Data"))
 
-    # Formatação do Código de Venda
-    if cod_venda_format:
-        data["Código de Venda"] = data["Código de Venda"].apply(lambda x: format(int(x), cod_venda_format) if pd.notnull(x) else "")
+    # Converte Código de Venda para o tipo de dado selecionado
+    if cod_venda_type == "Inteiro":
+        data["Código de Venda"] = data["Código de Venda"].astype(int, errors='ignore')
+    elif cod_venda_type == "Decimal":
+        data["Código de Venda"] = data["Código de Venda"].astype(float, errors='ignore')
+    elif cod_venda_type == "Data":
+        data["Código de Venda"] = pd.to_datetime(data["Código de Venda"], errors='coerce')
 
-    # Formatação do Código de Compra
-    if cod_compra_format:
-        data["Código de Compra"] = data["Código de Compra"].apply(lambda x: format(int(x), cod_compra_format) if pd.notnull(x) else "")
+    # Converte Código de Compra para o tipo de dado selecionado
+    if cod_compra_type == "Inteiro":
+        data["Código de Compra"] = data["Código de Compra"].astype(int, errors='ignore')
+    elif cod_compra_type == "Decimal":
+        data["Código de Compra"] = data["Código de Compra"].astype(float, errors='ignore')
+    elif cod_compra_type == "Data":
+        data["Código de Compra"] = pd.to_datetime(data["Código de Compra"], errors='coerce')
 
     # Aplica o filtro de NCM
     if ncm_filter:
