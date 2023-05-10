514febc81e6673e26bfde6dec39e47e5f2f2ee02
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-05-10 17:33:02 -0300

    Update AnáliseFiscal.py

--- AnáliseFiscal.py
+++ AnáliseFiscal.py
@@ -2,44 +2,47 @@ import pandas as pd
 import streamlit as st
 import glob
 
-def process_efd_file(file):
-    lines = file.readlines()
 
-    reg2 = []
-    reg3 = []
+def analisefiscal():
+    
+    def process_efd_file(file):
+        lines = file.readlines()
 
-    for line in lines:
-        decoded_line = line.decode('utf-8')
-        if decoded_line.startswith("|C100|"):
-            reg2.append(decoded_line.strip().split("|"))
-        elif decoded_line.startswith("|C170|"):
-            reg3.append(decoded_line.strip().split("|"))
+        reg2 = []
+        reg3 = []
 
-    df_c100 = pd.DataFrame(reg2[1:], columns=reg2[0])
-    df_c170 = pd.DataFrame(reg3[1:], columns=reg3[0])
+        for line in lines:
+            decoded_line = line.decode('utf-8')
+            if decoded_line.startswith("|C100|"):
+                reg2.append(decoded_line.strip().split("|"))
+            elif decoded_line.startswith("|C170|"):
+                reg3.append(decoded_line.strip().split("|"))
 
-    df_merged = pd.concat([df_c100.assign(dummy_key=1), df_c170.assign(dummy_key=1)], ignore_index=True).merge(
-        df_c100, on="dummy_key", how="outer", suffixes=("_c100", "_c170")
-    ).drop("dummy_key", axis=1)
+        df_c100 = pd.DataFrame(reg2[1:], columns=reg2[0])
+        df_c170 = pd.DataFrame(reg3[1:], columns=reg3[0])
 
-    return df_merged
+        df_merged = pd.concat([df_c100.assign(dummy_key=1), df_c170.assign(dummy_key=1)], ignore_index=True).merge(
+            df_c100, on="dummy_key", how="outer", suffixes=("_c100", "_c170")
+        ).drop("dummy_key", axis=1)
 
-def cabecalhoanalise():
-    # URL do link raw do arquivo Excel no GitHub
-    url = "https://github.com/mateus4422/cestcat/raw/cestcat/Cabe%C3%A7alho_An%C3%A1lise.xlsx"
+        return df_merged
 
-    # Carregue a planilha em um DataFrame do Pandas
-    df = pd.read_excel(url, engine='openpyxl')
+    def cabecalhoanalise():
+        # URL do link raw do arquivo Excel no GitHub
+        url = "https://github.com/mateus4422/cestcat/raw/cestcat/Cabe%C3%A7alho_An%C3%A1lise.xlsx"
 
-    # Exiba apenas o cabeçalho da planilha no Streamlit
-    st.write("Cabeçalho da planilha de análise fiscal:")
-    st.write(df.head(0))
+        # Carregue a planilha em um DataFrame do Pandas
+        df = pd.read_excel(url, engine='openpyxl')
 
-cabecalhoanalise()
+        # Exiba apenas o cabeçalho da planilha no Streamlit
+        st.write("Cabeçalho da planilha de análise fiscal:")
+        st.write(df.head(0))
 
-uploaded_files = st.file_uploader("Escolha os arquivos EFD em TXT", type="txt", accept_multiple_files=True)
+    cabecalhoanalise()
 
-if uploaded_files:
-    for file in uploaded_files:
-        processed_efd = process_efd_file(file)
-        st.write(processed_efd)
+    uploaded_files = st.file_uploader("Escolha os arquivos EFD em TXT", type="txt", accept_multiple_files=True)
+
+    if uploaded_files:
+        for file in uploaded_files:
+            processed_efd = process_efd_file(file)
+            st.write(processed_efd)
