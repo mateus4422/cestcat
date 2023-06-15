5012d01ba66318d3f940562ab432e41aecfeeeb6
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-06-15 15:19:02 -0300

    Update aliquota.py

--- aliquota.py
+++ aliquota.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import requests
 import io
-import streamlit as st
-
 
 def tb_aliquota():
     # URL do arquivo Excel
@@ -17,6 +15,9 @@ def tb_aliquota():
         # Leia os dados do Excel em um DataFrame do Pandas
         df = pd.read_excel(io.BytesIO(r.content), engine='openpyxl')
 
+        # Remova as vírgulas da coluna 'Código de Venda'
+        df['Código de Venda'] = df['Código de Venda'].str.replace(',', '')
+
         st.title("Tabela de Alíquota")
 
         # Filtros para pesquisar por 'Código de Venda'
@@ -27,6 +28,13 @@ def tb_aliquota():
 
         st.write(f"Resultados para 'Código de Venda': {cod_venda}")
         st.dataframe(filtered_df)
+
+        # Crie um link para baixar o DataFrame como um arquivo CSV
+        csv = filtered_df.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()
+        href = f'<a href="data:file/csv;base64,{b64}" download="aliquota.csv">Clique aqui para baixar o arquivo CSV filtrado</a>'
+        st.markdown(href, unsafe_allow_html=True)
+
     else:
         st.write(f"Não foi possível obter os dados do arquivo Excel. Código de status HTTP: {r.status_code}")
 
