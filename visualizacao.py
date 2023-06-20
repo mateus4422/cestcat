2aa3d06b2e483c483a14caa123208830bcd20946
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-06-20 16:33:22 -0300

    Update visualizacao.py

--- visualizacao.py
+++ visualizacao.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+import keyboard
 
 
 
