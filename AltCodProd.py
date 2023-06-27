f144eff9c5c7d677556e1548911bff5309d66645
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-06-27 15:19:03 -0300

    Update AltCodProd.py

--- AltCodProd.py
+++ AltCodProd.py
@@ -3,51 +3,47 @@ import mysql.connector
 import pandas as pd
 
 def altcodprod():
-    import streamlit as st
-import mysql.connector
-import pandas as pd
 
-
-def carregar_dados_tabela_conversao():
-    # Cria a conexão com o banco de dados
-    conn = mysql.connector.connect(
-        host="127.0.0.1",
-        user="mateus_ramos",
-        password="flamengo4422",
-        database="prodcat"
-    )
-
-    # Define a consulta SQL
-    sql = "SELECT * FROM tabela_conversao"
-
-    # Executa a consulta SQL e obtém os resultados
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    data = cursor.fetchall()
-
-    # Obtém os nomes das colunas
-    columns = [desc[0] for desc in cursor.description]
-
-    # Fecha o cursor
-    cursor.close()
-
-    # Cria o DataFrame com os dados e as colunas
-    df = pd.DataFrame(data, columns=columns)
-
-    # Fecha a conexão com o banco de dados
-    conn.close()
-
-    return df
-
-def exibir_dados_tabela_conversao():
-    # Carrega os dados da tabela_conversao
-    df = carregar_dados_tabela_conversao()
-
-    # Exibe os dados usando o Streamlit
-    st.title("Dados da tabela_conversao")
-    st.dataframe(df)
-
-if __name__ == "__main__":
-
-
-    altcodprod()
+    def carregar_dados_tabela_conversao():
+        # Cria a conexão com o banco de dados
+        conn = mysql.connector.connect(
+            host="127.0.0.1",
+            user="mateus_ramos",
+            password="flamengo4422",
+            database="prodcat"
+        )
+    
+        # Define a consulta SQL
+        sql = "SELECT * FROM tabela_conversao"
+    
+        # Executa a consulta SQL e obtém os resultados
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        data = cursor.fetchall()
+    
+        # Obtém os nomes das colunas
+        columns = [desc[0] for desc in cursor.description]
+    
+        # Fecha o cursor
+        cursor.close()
+    
+        # Cria o DataFrame com os dados e as colunas
+        df = pd.DataFrame(data, columns=columns)
+    
+        # Fecha a conexão com o banco de dados
+        conn.close()
+    
+        return df
+    
+    def exibir_dados_tabela_conversao():
+        # Carrega os dados da tabela_conversao
+        df = carregar_dados_tabela_conversao()
+    
+        # Exibe os dados usando o Streamlit
+        st.title("Dados da tabela_conversao")
+        st.dataframe(df)
+    
+    if __name__ == "__main__":
+    
+    
+        altcodprod()
