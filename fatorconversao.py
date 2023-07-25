48e9f26721c298c318b96f21bb0a783fae9563a1
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-07-25 10:34:30 -0300

    Update fatorconversao.py

--- fatorconversao.py
+++ fatorconversao.py
@@ -1,33 +1,29 @@
 import streamlit as st
 import pandas as pd
-import base64
-import io
 
 def fatorconv():
-    # URL do arquivo Excel
-    url = 'https://github.com/mateus4422/cestcat/raw/cestcat/Fator%20de%20convers%C3%A3o.xlsx'
+    # Link para o arquivo Excel hospedado no GitHub
+    url = "https://github.com/mateus4422/cestcat/raw/cestcat/Fator%20de%20convers%C3%A3o.xlsx"
 
-    # Tente baixar o arquivo e ler com o pandas
-    try:
-        df = pd.read_excel(url, engine='openpyxl')
-    except Exception as e:
-        st.error(f"Erro ao baixar o arquivo: {e}")
-        return
+    # Carregar o arquivo Excel em um DataFrame
+    df = pd.read_excel(url)
 
+    # Título do aplicativo
     st.title("Fator de Conversão")
 
-    # Filtro para pesquisar por Código do Produto
-    cod_produto = st.text_input("Digite o código do produto (Código do Produto):")
+    # Input para o filtro no "Código do Produto"
+    codigo_produto = st.text_input("Digite o Código do Produto para filtrar:")
 
-    # Filtrar o dataframe com base no Código do Produto inserido
-    filtered_df = df[df['Código do Produto'].astype(str).str.contains(cod_produto)]
-
-    # Selecionar apenas as colunas desejadas
-    columns_to_display = ['Código do Produto', 'Descrição', 'unimed', 'Fator', 'NCM', 'Produto ST']
-    filtered_df = filtered_df[columns_to_display]
-
-    st.write(f"Resultados para Código do Produto: {cod_produto}")
-    st.dataframe(filtered_df)
+    # Filtrar o DataFrame com base no Código do Produto
+    if codigo_produto:
+        df_filtered = df[df["Código do Produto"] == codigo_produto]
+        if not df_filtered.empty:
+            st.write("Resultados:")
+            st.write(df_filtered)
+        else:
+            st.warning("Nenhum resultado encontrado para o código do produto fornecido.")
+    else:
+        st.write("Digite um código de produto para começar.")
 
 if __name__ == "__main__":
     fatorconv()
