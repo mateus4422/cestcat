6749f54bcb5891b348a51bd0b580096ec442b56c
mateus4422 <130771211+mateus4422@users.noreply.github.com>
2023-07-25 10:53:00 -0300

    Update fatorconversao.py

--- fatorconversao.py
+++ fatorconversao.py
@@ -10,9 +10,19 @@ def fatorconv():
 
     st.title("Fator de Conversão")
 
-    # Exibir o DataFrame completo
-    st.write("DataFrame Completo:")
-    st.dataframe(df)
+    # Input para o filtro no "Código do Produto"
+    codigo_produto = st.text_input("Digite o Código do Produto para filtrar:")
+
+    # Filtrar o DataFrame com base no Código do Produto
+    if codigo_produto:
+        df_filtered = df[df["Código do Produto"].astype(str).str.contains(codigo_produto)]
+        if not df_filtered.empty:
+            st.write("Resultados:")
+            st.dataframe(df_filtered)
+        else:
+            st.warning("Nenhum resultado encontrado para o código do produto fornecido.")
+    else:
+        st.write("Digite um código de produto para começar.")
 
 if __name__ == "__main__":
     fatorconv()
